c42e3a7acdefc2aa590913c59639a1394f79d11c
SabihArshadR <sabih042@gmail.com>
2026-02-17 14:20:31 +0500

    location update

--- src/utils/location.ts
+++ src/utils/location.ts
@@ -7,8 +7,8 @@ export interface POI {
 }
 
 export const POIs: POI[] = [
-  // { id: 1, name: "POI1", lng: 0.384751, lat: 41.007177, radius: 5 },
-  { id: 1, name: "POI1", lng: 74.187050, lat: 32.161670, radius: 5 },
+  { id: 1, name: "POI1", lng: 0.384751, lat: 41.007177, radius: 5 },
+  // { id: 1, name: "POI1", lng: 74.187050, lat: 32.161670, radius: 5 },
   // { id: 1, name: "POI1", lng: 74.383200, lat: 31.580900, radius: 5 },
   { id: 2, name: "POI2", lng: 0.385187, lat: 41.008508, radius: 5 },
   { id: 3, name: "POI3", lng: 0.38491, lat: 41.009507, radius: 5 },
