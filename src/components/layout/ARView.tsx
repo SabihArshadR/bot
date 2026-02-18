d68ddb5be6d011eb258f48aec6162a3e5de40a4c
SabihArshadR <sabih042@gmail.com>
2026-02-18 12:38:13 +0500

    model rotation

--- src/components/layout/ARView.tsx
+++ src/components/layout/ARView.tsx
@@ -549,7 +549,8 @@ const Avatar = React.forwardRef((props: AvatarProps, forwardedRef: any) => {
     <AEntity
       ref={avatarRef}
       position={`${position.x} ${position.y} ${position.z}`}
-      rotation={`-10 ${deviceOrientation.alpha + userRotationY} 0`}
+      // rotation={`-10 ${deviceOrientation.alpha + userRotationY} 0`}
+      rotation={`-10 ${userRotationY} 0`}
       scale={`${userScale} ${userScale} ${userScale}`}
     >
       <AEntity
