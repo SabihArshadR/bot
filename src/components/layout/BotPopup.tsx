aeff191379df5107d7fc4ca67946ed27f7f33c78
SabihArshadR <sabih042@gmail.com>
2026-02-23 11:14:48 +0500

    popup setting

--- src/components/layout/BotPopup.tsx
+++ src/components/layout/BotPopup.tsx
@@ -18,53 +18,51 @@ const BotPopup = ({ onClose }: BotPopupProps) => {
   return (
     <div className="fixed inset-0 flex justify-center z-50 bg-white">
       <div className="w-full max-w-[400px] mx-auto">
-        <div className="bg-white flex flex-col justify-center items-center pb-10">
-          {/* <h1 className="text-2xl font-semibold text-[#160500] mt-10">
-            Amb el suport de:
-          </h1> */}
-          <Image
-            src={logo1}
-            alt="logo1"
-            className="w-[200px] h-[200px] object-cover"
-          />
-          <Image
-            src={logo2}
-            alt="log2"
-            className="w-[200px] h-[52px] object-cover"
-          />
-          <Image
-            src={logo3}
-            alt="log3"
-            className="w-[200px] h-[100px] object-cover"
-          />
-          <Image
-            src={logo4}
-            alt="log4"
-            className="w-[200px] h-[100px] object-cover"
-          />
-          {/* <h1 className="text-2xl font-semibold text-[#160500] mt-[50px]">
-            Creat per:
-          </h1> */}
-          <Image
-            src={logo5}
-            alt="log5"
-            className="w-[200px] h-[100px] object-cover"
-          />
-          <Image
-            src={logo6}
-            alt="log6"
-            className="w-[200px] h-[100px] object-cover"
-          />
-          <Image
-            src={logo7}
-            alt="logo7"
-            className="w-[200px] h-[100px] object-cover"
-          />
-          <Image
-            src={logo8}
-            alt="logo8"
-            className="w-[200px] h-[100px] object-cover"
-          />
+        <div className="bg-white flex justify-center min-h-screen items-center pb-10">
+          <div>
+            <Image
+              src={logo1}
+              alt="logo1"
+              className="w-[200px] h-[200px] object-cover"
+            />
+            <Image
+              src={logo2}
+              alt="log2"
+              className="w-[200px] h-[52px] object-cover"
+            />
+            <Image
+              src={logo3}
+              alt="log3"
+              className="w-[200px] h-[100px] object-cover"
+            />
+            <Image
+              src={logo4}
+              alt="log4"
+              className="w-[200px] h-[100px] object-cover"
+            />
+          </div>
+          <div>
+            <Image
+              src={logo5}
+              alt="log5"
+              className="w-[200px] h-[100px] object-cover mt-5"
+            />
+            <Image
+              src={logo6}
+              alt="log6"
+              className="w-[200px] h-[100px] object-cover"
+            />
+            <Image
+              src={logo7}
+              alt="logo7"
+              className="w-[200px] h-[100px] object-cover"
+            />
+            <Image
+              src={logo8}
+              alt="logo8"
+              className="w-[200px] h-[100px] object-cover"
+            />
+          </div>
         </div>
       </div>
     </div>
